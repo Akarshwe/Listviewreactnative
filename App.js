d1008898fd2553631592ef70059bcb7d2c467a0f
Akarshwe <akarsh348@gmail.com>
2022-12-15 13:03:28 +0530

    flatlist completed

--- App.js
+++ App.js
@@ -1,117 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const FlatListBasics = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Thrissur'},
+          {key: 'Thiruvananthapuram'},
+          {key: 'Palakkad'},
+          {key: 'Kannur'},
+          {key: 'Kozhikode'},
+          {key: 'Kottayam'},
+          {key: 'Kollam'},
+          {key: 'Alappuzha'},
+          {key: 'Ernakulam'},
+          {key: 'Pathanamthitta'},
+          {key: 'Wayanad'},
+          {key: 'idukki'},
+          {key: 'Malappuram'},
+          {key: 'Kasaragod'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+   flex: 1,
+   paddingTop: 22
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    padding: 10,
+    fontSize: 30,
+    height: 60,
   },
 });
 
-export default App;
+export default FlatListBasics;
--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 /**
- * @format
- */
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+
+
+
+
+// import {AppRegistry} from 'react-native';
+// import Listview1 from './Listview1';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => Listview1);
+
+
+
+
+// import {AppRegistry} from 'react-native';
+// import Customflaglist from './Customflaglist';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => Customflaglist);
+
+
+
+
+// import {AppRegistry} from 'react-native';
+// import Customflaglist from './Customflaglist';
+// import {name as appName} from './app.json';
+// import Flatlist from './Flatlist';
+// import CustomFlatlist2 from './Customflatlist2';
+
+// AppRegistry.registerComponent(appName, () => CustomFlatlist2);
+
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Customflatlist2 from './Customflatlist2';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Customflatlist2);
+
